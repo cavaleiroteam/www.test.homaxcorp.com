40c836c991eab3ca7b8064572525deb9fe21faeb
cavaleiroteam <teamcavaleiro@gmail.com>
2025-01-23 18:32:41 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+  <style>
+  </style>
+<title>Hacked by The Fuck Invisible</title>
+<link rel="icon" type="image/x-icon" href="<html>
+    <head>
+    <title>Hacked by Scarfer Nasher</title>
+    <link rel="icon" type="image/x-icon" href="https://exploit99.my.id/tools/others/uploads/67921be767b6c.png">
+    <meta content='Scarfer Nasher' name='description'/>
+    <meta content=':)' name='keywords'/>
+    <meta content=':?' name='Abstract'/>
+    <meta name="title" content="Hacked by Scarfer">
+    <meta name="description" content="Tidak ada keadilan untuk rakyat rakyat kecil"/>
+    <meta name="keywords" content="The Fuck Invisible"/>
+    <meta name="googlebot" content="index,follow"/>
+    <meta name="robots" content="all"/>
+    <meta name="robots schedule" content="auto"/>
+    <meta name="distribution" content="global"/>
+    <meta contact='#'/> <meta charset="utf-8">
+    <style>  
+    /* Your existing styles here */
+    </style>
+    </head>
+    <body align="center" oncontextmenu="return false" bgcolor="black">
+    <div class="stars">
+      <div class="twinkling">
+        <div class="flex-center position-ref full-height">
+          <div class="content">                                                                                                                  
+            <div class="text">
+              <font color="white">
+                <font size="4" face="Courier"><b><i>The Fuck Invisible</i></b></font>
+                    <body>
+                <br>
+               <br>
+                <img src="https://exploit99.my.id/tools/others/uploads/67921be767b6c.png" height="250">
+              </div>
+           <br>
+           <br>
+              <font size="4" face="Courier">Hacked by<b><i> Scarfer Nasher</i></b></font>
+                <br><br>     
+                <font size="3" face="Courier"><b><i>Korupsi bukanlah Solusi</i></b></font><br>
+                <font size="3" face="Courier"><b><i>#BunuhKoruptorAnjing!</i></b></font><br>
+               
+                <br>
+                            <font size="2" face="Courier"><b><i>Greetz : WatefakC2 - LopsusC2 - Hexan7 - Rio Haxor - ./LutzzXploit - ./Tyaa - AlwaysDoni - ./Razer</i></b></font>
+            </div>
+          </div></b></i>
+                    </body>
